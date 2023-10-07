0e35e320fd995b9181ffe6c9a99243daad456339
SiddikArim <arim99999@gmail.com>
2023-10-07 14:16:08 +0600

    single feature function and data display completed

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,29 +5,36 @@ const FeaturedJobs = () => {
     const [jobDatas, setJobDatas] = useState([])
     const [visData, setVisData] = useState(4)
     const prevVisData = visData
-    useEffect(()=>{
+    useEffect(() => {
         fetch('jobs.json')
-        .then(res=>res.json())
-        .then(data=>setJobDatas(data))
-    },[])
-    
+            .then(res => res.json())
+            .then(data => setJobDatas(data))
+    }, [])
+
     const showMore = () => {
-        setVisData(prevVisData=> visData+2)
+        setVisData(prevVisData => visData + 2)
     }
-       
+
     return (
-        <div>
-            <h2>Companys we got :{jobDatas.length} </h2>
-            {
-                jobDatas.slice(0, prevVisData).map(jD => <SingleFeatureJob
-                    key={jD.id}
-                    jD={jD}
-                    ></SingleFeatureJob> )
-            }
+        <div className='my-16'>
+            <div className='text-center'>
+                <h2 className='text-3xl font-semibold'>Iron-Man Jobs</h2>
+                <p className='text-center my-4'><span className='textColor font-semibold '>Empower Your Career:</span> Explore Opportunities in Your Ideal Job Category.</p>
+            </div>
+            <div className='grid grid-cols-2'>
+                {
+                    jobDatas.slice(0, prevVisData).map(jD => <SingleFeatureJob
+                        key={jD.id}
+                        jD={jD}
+                    ></SingleFeatureJob>)
+                }
+            </div>
             {/* show more sector */}
-            {
-                visData< jobDatas.length && (<button className='switch my-3' onClick={()=>showMore()}>Show More</button>) 
+           <div className='text-center'>
+           {
+                visData < jobDatas.length && (<button className='switch my-3' onClick={() => showMore()}>Show More</button>)
             }
+           </div>
         </div>
     );
 };
--- src/Components/SingleFeatureJob/SingleFeatureJob.jsx
+++ src/Components/SingleFeatureJob/SingleFeatureJob.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const SingleFeatureJob = () => {
+const SingleFeatureJob = ({ jD }) => {
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = jD
     return (
-        <div>
-            hi
+        <div className='border m-3 p-3 rounded border-inherit'>
+            <div>
+                <img src={logo} alt="" />
+                <h2 className='font-semibold pt-3'>{job_title}</h2>
+                <p className='font-light'>{company_name}</p>
+            </div>
+            <div className='textColor flex'>
+                <div className='border border-indigo-500 p-2 m-2 rounded'>
+                    {remote_or_onsite}
+                </div>
+                <div className='border border-indigo-500 p-2 m-2 rounded'>
+                    {job_type}
+                </div>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
